c8b2c49d32d0cc0d79d48e165a908eeebf47e2f2
kammanageswararao <41564015+kammanageswararao@users.noreply.github.com>
2019-12-29 13:09:02 +0530

    committed

--- FirstJavaProgramm.java
+++ FirstJavaProgramm.java
@@ -13,7 +13,8 @@ public class FirstJavaProgramm {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello wolrd sample programm");
+		System.out.println("Hello wolrd sample progr");
+		
 
 	}
 
