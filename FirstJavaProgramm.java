cdb274a394215647326b42b8206a668203d1765e
kammanageswararao <41564015+kammanageswararao@users.noreply.github.com>
2019-12-29 13:06:33 +0530

    commited

---
+++ FirstJavaProgramm.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author knageswarar2
+ *
+ */
+public class FirstJavaProgramm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello wolrd sample programm");
+
+	}
+
+}
