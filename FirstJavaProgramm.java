d231e758c9be245cb6e1f7113a5eaf2c3e5cac6c
kammanageswararao <41564015+kammanageswararao@users.noreply.github.com>
2019-12-29 13:23:15 +0530

    added priniting line

--- FirstJavaProgramm.java
+++ FirstJavaProgramm.java
@@ -14,6 +14,7 @@ public class FirstJavaProgramm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello wolrd sample progr");
+		System.out.println("second line commenting");
 		
 
 	}
